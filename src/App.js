6b3723c29e320474bc3d52ccd5057e801a80d29f
Hannah Sommer <hannah.j.sommer@gmail.com>
2022-10-16 00:42:13 -0400

    Edit head of webpage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-// import logo from './logo.svg';
-import React from "react";
-import AllRoutes from "./AllRoutes";
-import "./styling/App.css";
+import React from 'react';
+import AllRoutes from './AllRoutes';
+import './styling/App.css';
 
 function App() {
   return (
@@ -13,20 +12,3 @@ function App() {
 }
 
 export default App;
-
-/* <div className="App">
-<header className="App-header">
-  <img src={logo} className="App-logo" alt="logo" />
-  <p>
-    Edit <code>src/App.js</code> and save to reload.
-  </p>
-  <a
-    className="App-link"
-    href="https://reactjs.org"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    Learn React
-  </a>
-</header>
-</div> */
